e908fc2015c5a7a5691ae5ebe12ecedfb55710df
NicolleAmr <86444373+NicolleAmr@users.noreply.github.com>
2022-04-27 08:15:24 -0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const conexao = async() => {
+    //conexao local
+    //var bdlocal = await mongoose.connect('mongodb://localhost/ac1tri')
+    //conexao atlas
+    var atlas = await mongoose.connect('mongodb+srv://userAdmin:Nic1506@fiaptecnico.yk90q.mongodb.net/crud')
+}
+
+module.exports = conexao
