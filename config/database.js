efdc1578740a21a81c7014f998a7eb2a4934b643
NicolleAmr <86444373+NicolleAmr@users.noreply.github.com>
2022-05-02 08:50:58 -0300

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -4,8 +4,7 @@ const conexao = async() => {
     //conexao local
     //var bdlocal = await mongoose.connect('mongodb://localhost/ac1tri')
     //conexao atlas
-    var atlas = await mongoose.connect('mongodb+srv://userAdmin:Nic1506@fiaptecnico.yk90q.mongodb.net/crud')
+    var atlas = await mongoose.connect('mongodb+srv://userAdmin:Nic1506@fiaptecnico.yk90q.mongodb.net/test')
 }
 
 module.exports = conexao
-
--- routes/mygrid.js
+++ routes/mygrid.js
@@ -3,13 +3,13 @@ module.exports = (app)=>{
     var conexao = require('../config/database')
     //importar o modelo mygrid
     var modelo = require('../models/mygrid')
-
+    
     //abrir o formulário mygrid.ejs
     app.get('/mygrid',(req,res)=>{
         //conectar com o database
         conexao()
         //buscar todos os documentos da colecao mygrid
-        modelo.find()
+        modelo.find().sort({_id:-1})
         .then((modelo)=>{
             res.render('mygrid.ejs',{dados:modelo})
         })
@@ -17,7 +17,6 @@ module.exports = (app)=>{
             res.render('mygrid.ejs')
         })
     })
-
     //gravar as informações do formulário
     app.post('/mygrid',(req,res)=>{
         //conectar com o database
@@ -34,7 +33,6 @@ module.exports = (app)=>{
             res.send('Não foi possível gravar os dados no DB')
         })
     })
-
     //listar o documento para o excluir
     app.get('/mygrid_excluir', async(req,res)=>{
         //recuperar o id da barra de endereço
@@ -44,7 +42,6 @@ module.exports = (app)=>{
         //abrir a view mygrid_excluir e enviar a json do documento
         res.render('mygrid_excluir.ejs',{dados:procurar})
     })
-
     //listar o documento para o alterar
     app.get('/mygrid_alterar', async(req,res)=>{
         //recuperar o id da barra de endereço
@@ -54,16 +51,24 @@ module.exports = (app)=>{
         //abrir a view mygrid_alterar e enviar a json do documento
         res.render('mygrid_alterar.ejs',{dados:procurar})
     })
-
     //excluir documento da coleção atual
     app.get('/excluir_mygrid', async(req,res)=>{
         //recuperando o id da barra de endereço 
-        var id = req.query.id 
+        var id = req.query.id
         //excluindo o documento da coleção (n tem confirmação, n tem como desfazer o delet)
         var exluir = await modelo.findOneAndRemove({_id:id})
         //voltar para a página mygrid
         res.redirect('/mygrid')
-
     })
-
+       //alterar documento da coleção atual
+       app.post('/alterar_mygrid', async(req,res)=>{
+        //recuperando o id da barra de endereço 
+        var id = req.query.id
+        //recuperar as informações digitadas 
+        var dados = req.body
+         //alterando o documento da coleção (n tem confirmação, n tem como desfazer o delet)
+         var alterar = await modelo.findOneAndUpdate({_id:id}, {titulo:dados.titulo, texto:dados.texto})
+        //voltar para a página mygrid
+        res.redirect('/mygrid')
+    })
 }
